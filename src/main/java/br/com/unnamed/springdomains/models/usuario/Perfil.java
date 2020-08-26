df0a74834e9b74949e18a428781153c9b95d7f05
Lucas Vieira <lucas.vieira.dasilva@outlook.com>
2020-08-25 21:40:03 -0300

    Ajuste postgres

--- src/main/java/br/com/unnamed/springdomains/models/usuario/Perfil.java
+++ src/main/java/br/com/unnamed/springdomains/models/usuario/Perfil.java
@@ -1,21 +1,15 @@
 package br.com.unnamed.springdomains.models.usuario;
 
-import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Entity
-public class Usuario {
-
-private static final long serialVersionUID = 1L;
-
+public class Perfil {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
 	private String nome;
-	private String email;
-	private String senha;
 
 	public Long getId() {
 		return id;
@@ -31,25 +25,5 @@ private static final long serialVersionUID = 1L;
 
 	public void setNome(String nome) {
 		this.nome = nome;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getSenha() {
-		return senha;
-	}
-
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}
-
-	public static long getSerialversionuid() {
-		return serialVersionUID;
 	}
 }
